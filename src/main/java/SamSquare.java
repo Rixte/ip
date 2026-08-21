aed6d9f183b290063ebd1e130b5bb1465644246d
Xeriel <ikolpito@gmail.com>
2026-08-21 15:22:26 +0800

    Level 0 commit

---
+++ src/main/java/SamSquare.java
@@ -0,0 +1,17 @@
+public class SamSquare {
+    public static void main(String[] args) {
+        String banner = "      __________\n"
+                + "    /            \\\n"
+                + "   /   •     •    \\\n"
+                + "  |      ᴥ         |\n"
+                + "   \\    _____     /\n"
+                + "    \\____________/\n"
+                + "      ||    ||\n"
+                + "      ||____||";
+        System.out.println(banner);
+        System.out.println("Hello I am SamSquare.\n"
+                + "What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Byebye hope to see you again soon!");
+    }
+}
